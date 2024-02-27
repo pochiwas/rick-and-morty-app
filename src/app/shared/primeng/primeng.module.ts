993bfd04d91b12b2b3f4f04dcdf6572fbb468cc6
Elias Yanez <gtayanez@gmail.com>
2024-02-27 09:07:17 -0300

    armando esqueleto

---
+++ src/app/shared/primeng/primeng.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: []
+})
+export class PrimengModule { }
